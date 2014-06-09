e9736c76d139f00fe3dbfcfab67c7e13a4afeacc
Ignacio Ortega <ignacio.ortega@gmail.com>
2014-06-08 23:10:00 -0400

    add theme for vim

--- vim/install.sh
+++ vim/install.sh
@@ -4,4 +4,10 @@ set -e
 if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
+
+if [[ ! -d "$HOME/.vim/colors" ]]; then
+  mkdir -p "$HOME/.vim/colors"
+  cp vim/railscasts.vim "$HOME/.vim/colors"
+fi
+
 vim -u ~/.vimrc.bundles +BundleInstall +qa
